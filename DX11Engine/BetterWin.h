91695aef900a0b6547f59885fab7b966e8ce370c
Marcin Zalewski <marcinzal24@gmail.com>
2021-02-25 19:36:37 +0100

    Basic window setup

---
+++ DX11Engine/BetterWin.h
@@ -0,0 +1,46 @@
+/* ALWAYS INCLUDE THIS CLASS FIRST */
+#pragma once
+
+// Target Windows 7 or higher
+#define _WIN32_WINNT 0X0601
+#include <sdkddkver.h>
+
+// Following defines disable unused windows stuff. If you get 
+// weird errors try removing some/all defines.
+#define WIN32_LEAN_AND_MEAN
+#define NOGDICAPMASKS
+#define NOSYSMETRICS
+#define NOMENUS
+#define NOICONS
+#define NOSYSCOMMANDS
+#define NORASTEROPS
+#define OEMRESOURCE
+#define NOATOM
+#define NOCLIPBOARD
+#define NOCOLOR
+#define NOCTLMGR
+#define NODRAWTEXT
+#define NOKERNEL
+#define NONLS
+#define NOMEMMGR
+#define NOMETAFILE
+#define NOOPENFILE
+#define NOSCROLL
+#define NOSERVICE
+#define NOSOUND
+#define NOTEXTMETRIC
+#define NOWH
+#define NOCOMM
+#define NOKANJI
+#define NOHELP
+#define NOPROFILER
+#define NODEFERWINDOWPOS
+#define NOMCX
+#define NORPC
+#define NOPROXYSTUB
+#define NOIMAGE
+#define NOTAPE
+
+#define STRICT
+
+#include <Windows.h>
---
+++ DX11Engine/WinMain.cpp
@@ -0,0 +1,22 @@
+#include "Window.h"
+
+int CALLBACK WinMain(
+	HINSTANCE hInstance,
+	HINSTANCE hPrevInstance,
+	LPSTR     lpCmdLine,
+	int       nCmdShow)
+{
+	Window wnd(1280, 720, "DX11Engine");
+
+	MSG msg;
+	BOOL gResult;
+	while ((gResult = GetMessage(&msg, nullptr, 0, 0)) > 0)
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	if (gResult == -1) return -1;
+
+	return msg.wParam;
+}
